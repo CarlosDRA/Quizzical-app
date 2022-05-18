1596cb9bf4c7fe8a1a1ae1ab3618876114cead84
Carlos Regalado <cd.regaladoaguirre@hotmail.com>
2022-05-18 14:35:44 -0500

    Finished start page style

--- src/components/StartPage.jsx
+++ src/components/StartPage.jsx
@@ -1,9 +1,15 @@
 export default function StartPage(props){
     return(
-        <>
-            <h1>Quizzical</h1>
-            <p>Some description if needed</p>
-            <button onClick={props.start}>Start quiz</button>
-        </>    
+        <div className="start--page">
+            <h1 className="title">Quizzical</h1>
+            <p className="text">Test your general knowledge with this quick and fun quiz!<br/>
+            Press the button to start and be sure to select the correct answers.</p>
+            <button 
+                onClick={props.start}
+                className="button"
+            >
+                Start quiz
+            </button>
+        </div>    
     )
 }
